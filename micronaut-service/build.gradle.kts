b2b9562a43697d9505aa16c8820caba18f52243d
Roman Kudryashov <rskudryashov@gmail.com>
2019-03-16 20:10:47 +0300

    Update dependencies

--- micronaut-service/build.gradle.kts
+++ micronaut-service/build.gradle.kts
@@ -1,5 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val micronautVersion: String by project
 val jacksonVersion: String by project
